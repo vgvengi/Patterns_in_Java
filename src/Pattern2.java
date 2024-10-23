f430b003e495cb83adab459338118d1002153c28
vgvengi <vengateshvg1123@gmail.com>
2024-10-23 21:53:48 +0530

    pattern2

---
+++ src/Pattern2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Pattern2 {
+
+    /*
+    input n=5
+    output
+    *
+    **
+    ***
+    ****
+    *****
+     */
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
